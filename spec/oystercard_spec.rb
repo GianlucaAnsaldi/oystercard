31696c0201266d5a432e1bf73402542628db6f5c
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-08-31 16:33:06 +0100

    completed chapter 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,22 @@
-decribe Oystercard do
+require 'oystercard'
+
+describe Oystercard do
+
+  it "responds to balance method" do 
+    oystercard = Oystercard.new
+    expect(oystercard).to respond_to(:balance)
+  end
+
+  it "should have a balance of 0" do
+    oystercard = Oystercard.new 
+    expect(oystercard.balance).to eq(0)
+  end
+
 end
+
+
+
+
+#In order to use public transport
+#As a customer
+#I want money on my card
